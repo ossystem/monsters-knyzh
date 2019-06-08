f6934f1b42bf9d1c5670c5b27c286fc3fe1420c1
i.knyzh <i.knyzh@ossystem.com.ua>
2019-06-08 18:34:07 +0300

    add constants

---
+++ src/constants.js
@@ -0,0 +1,54 @@
+export default {
+    serverUrl: '',
+    token: '12345678',
+    notSelected: "there's nothing to do. You must answer that question",
+    result: "Excellent, congratulations, you're a monster",
+    possibleAnswers: {
+        2: {
+            1: {
+                title: 'Are you afraid of the dark?',
+                questions: [
+                    `I'm not scared at all`,
+                    'Sometimes',
+                    'Never',
+                    'What a stupid question!'],
+                img: '/img/page_3_monster.png',
+                inputType : 'RadioButton'
+            },
+            2: {
+                title: 'What do you prefer?',
+                questions: [
+                    'Fuf',
+                    'Snake skin',
+                    'Slick skin',
+                    'Velvet'
+                ],
+                img: '/img/page_4_monster.png',
+                inputType : 'CheckboxInput'
+            }
+        },
+        3: {
+            1: {
+                title: 'Are you a monster day or a night monster',
+                questions: [
+                    'Day monster',
+                    'Night monster'
+                ],
+                img: '/img/page_7_monster.png',
+                inputType : 'SwitchInput'
+            }
+        },
+        4: {
+            1: {
+                title: "You'd rather be a bad or a good monster",
+                questions: [
+                    'Bad monster',
+                    'Good monster'
+
+                ],
+                img: '/img/page_8_monster.png',
+                inputType : 'SliderInput'
+            }
+        }
+    }
+};
